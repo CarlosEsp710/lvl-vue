ae0e94cab02792e96f23beeebc6b2ebf39d65d62
Carlos Espejel <carlosespejel71@gmail.com>
2021-02-24 15:57:19 -0600

    Creación de componente VUE para genrear ideas

---
+++ app/Http/Controllers/IdeaController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Idea;
+use Illuminate\Http\Request;
+
+class IdeaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Idea::orderBy('id', 'DESC')->get();
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'description' => 'required'
+        ]);
+
+        Idea::create($request->all());
+
+        return;
+    }
+}
---
+++ resources/js/components/IdeaComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2 class="text-center">Catura tus Ideas</h2>
+    <div class="card card-body bg-light">
+      <h4 class="text-start">¿En qué estás pensando?</h4>
+      <form>
+        <div class="input-group sm-3">
+          <input type="text" class="form-control" />
+          <button
+            type="submit"
+            class="btn btn-outline-secondary"
+            id="button-addon2"
+          >
+            Agregar
+          </button>
+        </div>
+      </form>
+      <hr />
+      <ul class="list-unstyled">
+        <li>
+          <p class="fs-6 text-start">
+            <small class="text-muted">hace un minuto</small>
+            <em>Mi nueva idea</em>
+          </p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  },
+};
+</script>
